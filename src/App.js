0e007d7719912dffdb4884fda1d80b33f7167e3a
deep-rawat <deepakgamer9521@gmail.com>
2023-05-10 18:00:12 +0530

    path addd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
     <Router basename="/myreactapp">
            <Routes>
-                 <Route exact path='/' element={< Home />}></Route>
+                 <Route exact path='/myreactapp' element={< Home />}></Route>
                  <Route exact path='/about' element={< About />}></Route>
                  <Route exact path='/contact' element={< Contact />}></Route>
           </Routes>
