71d8b6a362ed78c5dfb14e93258602a94ec09107
deep-rawat <deepakgamer9521@gmail.com>
2023-05-10 16:59:05 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
 import {useState} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {Route, BrowserRouter as Router, Routes, Link} from 'react-router-dom';
+import {Home} from './components/Home';
+import {About} from './components/About';
+import {Contact} from './components/Contact';
 
 function App() {
-  const [name, setName] = useState('Name');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div class="form-group">
-    <label htmlFor="exampleInputEmail1">Enter Your Name</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name" onChange={(e) => setName(e.target.value)}/>
-  </div>
-        <p>
-          Sunil Ji
-          <br />
-          Dev Ji
-          <br />
-          Abrar Ji
-          <br />
-          Kamlesh Ji
-          <br />
-          Vinayak Ji
-          <br />
-          Balwant ji
-        </p>
-        <p>
-          <i>{name}</i> ki Shaadi me jlul jlul aana
-        </p>
-        <p>Baal Manuhar: <strong>Balwant</strong></p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+           <div>
+            <ul className="App-header">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+            </ul>
+           <Routes>
+                 <Route exact path='/' element={< Home />}></Route>
+                 <Route exact path='/about' element={< About />}></Route>
+                 <Route exact path='/contact' element={< Contact />}></Route>
+          </Routes>
+          </div>
+       </Router>
+  
   );
 }
 
